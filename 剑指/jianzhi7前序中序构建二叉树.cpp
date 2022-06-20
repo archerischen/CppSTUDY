0a6b64eb9d071fd738bb3d7589b5e26a9684453a
archerischen <62463294+archerischen@users.noreply.github.com>
2022-06-20 23:34:06 +0800

    Update jianzhi7前序中序构建二叉树.cpp

--- 剑指/jianzhi7前序中序构建二叉树.cpp
+++ 剑指/jianzhi7前序中序构建二叉树.cpp
@@ -1,66 +1,86 @@
 //
-// Created by 陈某林 on 2022/5/14.
+// Created by 陈某林 on 2022/6/20.
 //
 
-#include "jianzhi7.h"
+#include "mytest_test.h"
 #include <iostream>
 #include <vector>
 #include <queue>
 #include <algorithm>
 using namespace std;
-struct TreeNode{
+struct Bitree{
     int num;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : num(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : num(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : num(x), left(left), right(right) {}
+    Bitree *Left;
+    Bitree *Right;
+    Bitree():num(0),Left(nullptr),Right(nullptr){}
+    Bitree(int a):num(a),Left(nullptr),Right(nullptr){}
+    Bitree(int a,Bitree *left,Bitree *right):num(a),Left(left),Right(right){}
 };
-TreeNode* Bildtree(vector<int>&, vector<int>&);
-void layerprint(TreeNode*);
-void jianzhi7() {
+Bitree* CreateTree(vector<int> &,vector<int> &);
+void LayerPrint(Bitree *);
+void preorderPrint(Bitree *);
+void inorderPrint(Bitree *);
+void backorderPrint(Bitree *);
+void mytest_test() {
     vector<int> preorder={1,2,4,7,3,5,6,8};
-    vector<int> inorder ={4,7,2,1,5,3,8,6};
-    TreeNode *base = Bildtree(preorder,inorder);
-    layerprint(base);//print in layer order
+    vector<int> inorder={4,7,2,1,5,3,8,6};
+    Bitree *first = CreateTree(preorder,inorder);
+    LayerPrint(first);
+    cout<<endl;
+    preorderPrint(first);
+    cout<<endl;
+    inorderPrint(first);
+    cout<<endl;
+    backorderPrint(first);
+    cout<<endl;
 }
-TreeNode* Bildtree(vector<int> &preorder, vector<int> &inorder){
+Bitree* CreateTree(vector<int> &preorder,vector<int> &inorder){
     if(preorder.size()==0)
         return nullptr;
     if(preorder.size()==1){
-        TreeNode *p = new TreeNode(preorder[0]);
+        Bitree *p = new Bitree(preorder[0]);
         return p;
     }
-    int a = preorder[0];//now root
-    vector<int> leftpreorder,rightinorder;//
-    rightinorder.assign(inorder.begin(),find(inorder.begin(),inorder.end(),a));
-    inorder.erase(inorder.begin(),find(inorder.begin(),inorder.end(),a)+1);
-    //"assign" left is closed && right is open
-    preorder.erase(preorder.begin());//remove root
-    leftpreorder.assign(preorder.begin(),preorder.begin()+rightinorder.size());
-    preorder.erase(preorder.begin(),preorder.begin()+rightinorder.size());
-    TreeNode *p = new TreeNode(a, Bildtree(leftpreorder,rightinorder), Bildtree(preorder,inorder));
+    int num = preorder[0];
+    vector<int> leftpreorder,leftinorder;
+    leftinorder.assign(inorder.begin(),find(inorder.begin(),inorder.end(),num));
+    inorder.erase(inorder.begin(),inorder.begin()+leftinorder.size()+1);
+    preorder.erase(preorder.begin());
+    leftpreorder.assign(preorder.begin(),preorder.begin()+leftinorder.size());
+    preorder.erase(preorder.begin(),preorder.begin()+leftinorder.size());
+    Bitree *p = new Bitree(num,CreateTree(leftpreorder,leftinorder),CreateTree(preorder,inorder));
     return p;
 }
-void layerprint(TreeNode* node){
-    queue<TreeNode*> myqueue;
-    myqueue.push(node);
+void LayerPrint(Bitree *root){
+    queue<Bitree*> myqueue;
+    myqueue.push(root);
     while(!myqueue.empty()){
-        TreeNode* now = myqueue.front();
-        if(now != NULL)
-        {
-            cout<<now->num<<" ";
-            if(now->left)
-                myqueue.push(now->left);
-            //else
-                //myqueue.push(NULL);
-            if(now->right)
-                myqueue.push(now->right);
-            //else
-                //myqueue.push(NULL);
-        }
-        else
-            cout<<"NULL"<<" ";
+        cout<<myqueue.front()->num<<" ";
+        if(myqueue.front()->Left)
+            myqueue.push(myqueue.front()->Left);
+        if(myqueue.front()->Right)
+            myqueue.push(myqueue.front()->Right);
         myqueue.pop();
+    }
+}
+void preorderPrint(Bitree *root){
+    if(root!= nullptr){
+        cout<<root->num<<" ";
+        preorderPrint(root->Left);
+        preorderPrint(root->Right);
+    }
+}
+void inorderPrint(Bitree *root){
+    if(root!= nullptr){
+        inorderPrint(root->Left);
+        cout<<root->num<<" ";
+        inorderPrint(root->Right);
+    }
+}
+void backorderPrint(Bitree *root){
+    if(root!= nullptr){
+        backorderPrint(root->Left);
+        backorderPrint(root->Right);
+        cout<<root->num<<" ";
     }
 }
